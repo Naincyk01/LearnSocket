46f9d765a24f12446178f818a330b170d8aeb8f7
Naincy <naincyk15042004@gmail.com>
2024-07-11 23:46:47 +0530

    Real-time chat

--- server/app.js
+++ server/app.js
@@ -1,13 +1,56 @@
 import express from "express";
 import { Server } from "socket.io";
+import {createServer} from "http";
+import cors from "cors";
+
 const port = 3000;
 
 const app = express();
-const server = new Server(server)
+const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET","POST"],
+        credentials:true,
+    }
+});
+
+
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
 
 app.get("/",(req,res)=>{
     res.send("Hello world!");
 });
-app.listen(port,()=>{
+
+
+io.on("connection",(socket)=>{
+console.log("User connected!",socket.id);
+
+socket.on("message", ({ room, message }) => {
+  console.log({ room, message });
+  socket.to(room).emit("receive-message", message);
+
+});
+
+socket.on("join-room", (room) => {
+  socket.join(room);
+  console.log(`User joined room ${room}`);
+});
+
+// socket.emit("welcome",`Welcome to the server,${socket.id}`);
+// socket.broadcast.emit("welcome",`${socket.id} joined the server`);
+
+socket.on("disconnect",()=>{
+    console.log("user disconnected",socket.id);
+})    
+})
+
+server.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 })
